02000e7564ba0305a197aa0f3f05be600c991401
tlwsy <2307615383@qq.com>
2026-09-22 20:38:52 +0800

    chore(license): include deployment entrypoint notice

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# SPDX-FileCopyrightText: 2026 Clipo contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
 set -eu
 alembic -c /app/backend/alembic.ini upgrade head
 export CLIPO_BIND_HOST=0.0.0.0
